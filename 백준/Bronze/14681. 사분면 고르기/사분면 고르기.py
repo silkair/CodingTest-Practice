45f26997e16aab62fc57e0c8348ea3f31f277b84
이상진 <silkair1234@gmail.com>
2024-11-02 01:40:12 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+
+if a>0 :
+    if b>0 :
+        print(1)
+    elif b<0 :
+        print(4)
+if a<0 :
+    if b>0 :
+        print(2)
+    elif b<0 :
+        print(3)        
