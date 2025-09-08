6b2bf5acef67e3fbd8a258aa896dbbebecda74aa
이상진 <silkair1234@gmail.com>
2025-09-09 01:02:54 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.29 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,48 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        int n = array.length;
-        int[] max = new int[n];
-        
-        for(int i = 0; i < n; i++) {
-            int count = 0;
-            for(int j = 0; j < n; j++) {
-                if(array[i] == array[j]) {
-                count++;
-                }
-            }
-                max[i] = count;
-        }
-    
-        int best = 0;
-        for (int i = 0; i < n; i++) {
-            if (max[i] > best) {
-                best = max[i];
-            }
-        }
-        
-        int mode = -1;
-        int RealBest = 0;
-        
-         for (int i = 0; i < n; i++) {
+        int maxCount = 0;
+        int answer = 0;
 
-            boolean first = true;
-            
-            for (int k = 0; k < i; k++) {
-                if (array[k] == array[i]) {
-                    first = false;
-                    break;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+           
+        for(int number : array){
+            int count = map.getOrDefault(number, 0) + 1;
+            if(count > maxCount){
+                maxCount = count;
+                answer = number;
             }
-             
-            if (!first) continue;
-
-            if (max[i] == best) {
-                RealBest++;
-                mode = array[i];
-                if (RealBest >= 2) return -1;
+            else  if(count == maxCount){
+                answer = -1;
             }
+            map.put(number, count);
         }
-        return mode;
+        return answer;
     }
 }
